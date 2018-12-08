79dd3d3fd564664bec5dfc155df6f32c5f0748e9
Bryce Bjork <brycedbjork@gmail.com>
2018-12-08 15:56:02 -0500

    api with promise example

--- src/api.js
+++ src/api.js
@@ -34,50 +34,50 @@ export const CreatePost = (userId, groupName, content) => {
 };
 
 
-export const createGroup = (groupName) => {
+export const CreateGroup = (GroupName) => {
     // firestore.collection()
+    return new Promise((resolve, reject) => {
+        firestore.collection("groups").add({
+            groupName: groupName,
+            timestamp: firebase.firestore.Timestamp.now().toMillis()
+        }).then(doc => {
+            resolve(doc.id)
+        }).catch(error => reject(error))
+    })
+};
 
-    firestore.collection("groups").add({
-        groupName: groupName,
-    }).then(function (res) {
-        firestore.collection("groups/").doc(res.id).update({
-            // groupId: res.id,
-            timeStamp:firebase.firestore.Timestamp.now().toMillis()
-        })
-    });};
+export const GetUsersPosts = () => {
+    var userId = firebase.auth().currentUser.uid;
+    let firestore = firebase.firestore();
+    return firestore.collection('posts/').where("userId", "==", userId).get()
+    // console.log(firestore.collection('posts/').where("userId", "==",userId).get().then(function(snap){
+    //     snap.forEach(function(post){
+    //         console.log(post.id,"=>",post.data())
+    //     })
+};
 
-    export const GetUsersPosts = () => {
-        var userId = firebase.auth().currentUser.uid;
-        let firestore = firebase.firestore();
-        return firestore.collection('posts/').where("userId", "==", userId).get()
-        // console.log(firestore.collection('posts/').where("userId", "==",userId).get().then(function(snap){
-        //     snap.forEach(function(post){
-        //         console.log(post.id,"=>",post.data())
-        //     })
-    };
-
-    export const GetAllPosts = () => {
-        let firestore = firebase.firestore();
-        return firestore.collection('posts/').get()
-    };
-
-
-    export const Login = () => {
-        return firebase.auth().signInWithPopup(provider)
-    };
+export const GetAllPosts = () => {
+    let firestore = firebase.firestore();
+    return firestore.collection('posts/').get()
+};
 
 
-    export const CreateProfile = (userId, name_in) => {
+export const Login = () => {
+    return firebase.auth().signInWithPopup(provider)
+};
 
-return firestore.collection("users").doc(userId).set({
-    name: name_in
-})
-.then(function() {
-    console.log("Document successfully written!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
+
+export const CreateProfile = (userId, name_in) => {
+
+    return firestore.collection("users").doc(userId).set({
+        name: name_in
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
 };
 
 
