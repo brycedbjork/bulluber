bf5d7d01154290be94ccb613afde378c54856c7e
Bryce Bjork <brycedbjork@gmail.com>
2018-12-08 14:58:07 -0500

    header body

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, {Component} from 'react';
 import {CreatePost, Login, Logout, CreateProfile} from "./api"
 import firebase from "firebase"
 import styled from "styled-components"
+import { colors } from "./lib/styles"
 
 const Wrapper = styled.div`
   width: 100%;
   min-height: 800px;
   box-sizing: border-box;
-  padding: 40px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 `;
 
@@ -21,9 +21,9 @@ const CreatePostButton = styled.button`
 `;
 
 const LoginButton = styled.button`
-  margin: 40px;
-  font-size: 24px;
-  padding: 10px;
+  font-size: 20px;
+  border: none;
+  font-weight: 600;
 `;
 
 const LogoutButton = styled.button`
@@ -52,6 +52,62 @@ const ContentInput = styled.textarea`
   border-radius: 3px;
 `;
 
+const Header = styled.div`
+  height: 80px;
+  box-sizing: border-box;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 40px;
+  padding-right: 40px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: rgba(19, 23, 39, 0.1) 0px 4px 10px 0px;
+`
+
+const LeftHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`
+
+const RightHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${colors.nearBlack};
+`
+
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${colors.gray};
+  margin-left: 10px;
+`
+
+const GoogleImage = styled.img`
+  height: 30px;
+  width: auto;
+  margin-top: 5px;
+`
+
+const Groups = styled.div`
+  
+`
+
+const Posts = styled.div`
+
+`
+
 class App extends Component {
 
     constructor(props) {
@@ -60,9 +116,7 @@ class App extends Component {
         this.state = {
             currentUserUID: 0,
             content: "",
-
             group: "General",
-
             isLoggedIn: false
 
         };
@@ -116,30 +170,32 @@ class App extends Component {
     };
 
     render() {
-      let button; 
-      if (this.state.isLoggedIn === false) {
-        button = <LoginButton 
-                  value="Submit" onClick={() => {
-                    {
-                      this.handleLogin();
-                    }
-                  }}>
-                    Login with Google
-                  </LoginButton>
-      } else {
-        button = <LogoutButton 
-                    value="Submit" onClick={() => {
-                      {
-                        this.handleLogout()
-                      }
-                    }}>
-                      Logout
-                    </LogoutButton>
-      }
         return (
             <div className="Login">
                 <Wrapper>
-                    {button}
+                  <Header>
+                    <LeftHeader>
+                      <Title>bulluber</Title>
+                      <SubTitle>community talk.</SubTitle>
+                    </LeftHeader>
+                    <RightHeader>
+                      <LoginButton 
+                        value="Submit" onClick={() => this.state.isLoggedIn ? this.handleLogout : this.handleLogin}>
+                          {this.state.isLoggedIn && "logout"}
+                          {!this.state.isLoggedIn && (
+                            <GoogleImage src={require("./assets/google.png")}/>
+                          )}
+                      </LoginButton>
+                    </RightHeader>
+                  </Header>
+                  <Body>
+                    <Groups>
+                      
+                    </Groups>
+                    <Posts>
+                      
+                    </Posts>
+                  </Body>
                     <form>
                         <label>
                             Group:
---
+++ src/lib/styles.js
@@ -0,0 +1,7 @@
+export const colors = {
+	nearBlack: "#131727",
+	gray: "#9E9E9E",
+	blue: "#314AA4",
+	lightBlue: "rgba(19, 23, 39, 0.3)",
+	blueBackground: "rgba(49, 74, 164, 0.05)"
+}
