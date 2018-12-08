a69f01ce4a10a1cd5af6978b5ca552168ae95c69
Bryce Bjork <brycedbjork@gmail.com>
2018-12-08 15:40:05 -0500

    app.js restore

---
+++ src/App.js
@@ -0,0 +1,244 @@
+import React, {Component} from 'react';
+import {createGroup, CreatePost, GetAllPosts, GetUsersPosts, Login, Logout, CreateProfile} from "./api"
+import firebase from "firebase"
+import styled from "styled-components"
+import { colors } from "./lib/styles"
+import Groups from "./Groups"
+import Posts from "./Posts"
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 800px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const CreatePostButton = styled.button`
+  margin: 40px;
+  font-size: 24px;
+  padding: 10px;
+`;
+
+const LoginButton = styled.button`
+  font-size: 20px;
+  border: none;
+  font-weight: 600;
+`;
+
+const LogoutButton = styled.button`
+  margin: 40px;
+  font-size: 24px;
+  padding: 10px;
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const ContentInput = styled.textarea`
+  padding: 0.5em;
+  margin: 0.5em;
+  width:500px;
+  height:300px;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const Header = styled.div`
+  height: 80px;
+  box-sizing: border-box;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 40px;
+  padding-right: 40px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: rgba(19, 23, 39, 0.1) 0px 4px 10px 0px;
+`
+
+const LeftHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`
+
+const RightHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${colors.nearBlack};
+`
+
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${colors.gray};
+  margin-left: 10px;
+`
+
+const GoogleImage = styled.img`
+  height: 30px;
+  width: auto;
+  margin-top: 5px;
+`
+
+const Body = styled.div`
+
+`
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUserUID: 0,
+            content: "",
+            group: "General",
+            isLoggedIn: false
+
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
+
+    handleLogin = () => {
+        let uid = -1;
+        let name = "";
+        var that = this;
+        Login().then(function (result) {
+            uid = result.user.uid;
+            name = result.user.displayName;
+            console.log(result)
+            that.setState({currentUserUID: uid, isLoggedIn: true});
+            // CreateProfile(that.state.currentUserUID, name)
+        }).catch(function (error) {
+            alert(error.message);
+        });
+
+    };
+
+    handleLogout = () => {
+        alert("Logging out!");
+        Logout();
+        this.setState({isLoggedIn: false}); 
+        console.log("logout is: ", this.state.isLoggedIn);
+    };
+
+
+    handleGetUsersPosts = () => {
+        GetUsersPosts().then(function(snap){
+                snap.forEach(function(post){
+                    console.log(post.id,"=>",post.data())
+                })
+    })};
+
+
+    handleGetAllPosts = () => {
+        GetAllPosts().then(function(snap){
+            snap.forEach(function(post){
+                console.log(post.id,"=>",post.data())
+                return(
+                  <div>I LOVE BUBBLES</div>
+                  )
+            })
+        })};
+
+    //
+    // handleMessagesDisplay = () => {
+    //     var userId = firebase.auth().currentUser.uid;
+    //     let firestore = firebase.firestore();
+    //     console.log(firestore.collection('posts/').where("userId", "==",userId).get().then(function(snap){
+    //         snap.forEach(function(post){
+    //             console.log(post.id,"=>",post.data())
+    //         })
+    //     }))
+    // };
+
+    render() {
+        return (
+            <div className="Login">
+                <Wrapper>
+                  <Header>
+                    <LeftHeader>
+                      <Title>bulluber</Title>
+                      <SubTitle>community talk.</SubTitle>
+                    </LeftHeader>
+                    <RightHeader>
+                      <LoginButton 
+                        value="Submit" onClick={() => {
+                          if (this.state.isLoggedIn) {
+                            this.handleLogout()
+                          }
+                          else {
+                            this.handleLogin()
+                          }
+                        }}>
+                          {this.state.isLoggedIn && "logout"}
+                          {!this.state.isLoggedIn && (
+                            <GoogleImage src={require("./assets/google.png")}/>
+                          )}
+                      </LoginButton>
+                    </RightHeader>
+                  </Header>
+                  <Body>
+                    <Groups/>
+                    {this.state.isLoggedIn && <Posts/>}
+                  </Body>
+
+                  <CreatePostButton onClick={() => {
+                      this.handleGetAllPosts(this.state.currentUserUID);
+                  }}>
+                      View all posts
+                  </CreatePostButton>
+                  <CreatePostButton onClick={() => {
+                      this.handleGetUsersPosts(this.state.currentUserUID);
+                  }}>
+                      View my posts
+                  </CreatePostButton>
+                  <CreatePostButton onClick={() => {
+                      createGroup(this.state.group);
+                  }}>
+                      Make group
+                  </CreatePostButton>
+
+
+                </Wrapper>
+            </div>
+        );
+    }
+}
+
+
+export default App;
